c17d7d5c20e4f2c1ca7eed7dda202eb3c1e3ef93
Nate Jacobs <nate@natejacobs.org>
2012-01-24 03:33:34 -0700

    Add wanted list shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -1,12 +1,13 @@
 <?php
 
-$brickset_shortcode = new BricksetAPIShortcode();
+new BricksetAPIShortcode;
 
 class BricksetAPIShortcode
 {
 	public function __construct()
 	{
 		add_shortcode( 'bs_set', array( __CLASS__, 'get_set' ) );
+		add_shortcode( 'bs_my_wanted', array( __CLASS__, 'my_wanted' ) );
 	}
 	
 	public function get_set( $atts )
@@ -25,6 +26,30 @@ class BricksetAPIShortcode
 		$return .= '<strong>Theme: </strong>'.$result->theme.'<br>';
 		$return .= '<strong>Subtheme: </strong>'.$result->subtheme.'<br>';
 		$return .= '<strong>Pieces: </strong>'.$result->pieces.'<br>';
+		
+		return $return;
+	}
+	
+	public function my_wanted()
+	{
+		global $current_user;
+		get_currentuserinfo();
+		$user_id = $current_user->ID;
+		$brickset = new BricksetAPIFunctions();
+		$results = $brickset->get_wanted( $user_id );
+		
+		$return = '<h2>My Wanted List</h2>';
+		$return .= '<table><th>Image</th><th>Set Name</th><th>Set Number</th><th>Pieces</th>';	
+		foreach ( $results as $result )
+		{
+			$return .= '<tr>';
+				$return .= '<td><img src="'.$result->thumbnailURL.'"></td>';
+				$return .= '<td>'.$result->setName.'</td>';
+				$return .= '<td>'.$result->number.'</td>';
+				$return .= '<td>'.$result->pieces.'</td>';
+			$return .= '</tr>';
+		}
+		$return .= '</table>';		
 		
 		return $return;
 	}
