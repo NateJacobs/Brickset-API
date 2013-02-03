c0e98dfb0d347f205ec5f6f966dd36b651379235
Nate Jacobs <njelmo@gmail.com>
2013-02-02 23:27:45 -0700

    Add settings page

--- admin/class-settings-page.php
+++ admin/class-settings-page.php
@@ -2,108 +2,121 @@
 
 // 'brickset_apikey'
 
-class BricksetAPIAdmin extends BricksetAPIFunctions
+class BricksetAPISettingsPage
 {
 	public function __construct()
 	{
 		add_action( 'admin_menu', array( __CLASS__, 'add_brickset_submenu' ) );
-		add_action( 'show_user_profile', array( __CLASS__, 'add_user_profile_fields' ) );
-		add_action( 'edit_user_profile', array( __CLASS__, 'add_user_profile_fields' ) );
-		add_action( 'personal_options_update', array( __CLASS__, 'save_user_profile_fields' ) );
-		add_action( 'edit_user_profile_update', array( __CLASS__, 'save_user_profile_fields' ) );
-	}
-	/** 
-	 *	Login Service Method
-	 *
-	 *	Authenticates a user with Brickset and returns a hash.
-	 *	The hash is then stored as a meta value with the key of 'brickset_hash'
-	 *	in the *_usersmeta table.
-	 *
-	 *	@author		Nate Jacobs
-	 *	@since		0.1
-	 *
-	 *	@param	int 	$user_id
-	 *	@param	string 	$username
-	 *	@param	string	$password
-	 */
-	public function brickset_login( $user_id, $username, $password )
-	{
-		$user = get_userdata( $user_id );
-		
-		$params = 'u='.$username.'&p='.$password;
-	
-		parent::remote_request( 'login', $params );
-		$user_hash = $this->results;
-		
-		try
-		{
-			if ( $this->httpcode != 200 )
-				throw new Exception ( $this->error_msg );
-			update_user_meta( $user->ID, 'brickset_user_hash',  $user_hash );
-		}
-		catch ( Exception $e ) 
-		{
-			echo $e->getMessage();
-		}
+		add_action( 'admin_init', array( __CLASS__, 'settings_init' ) );
 	}
 	
 	/** 
-	 *	
-	 *
-	 *
-	 *
-	 *	@author		Nate Jacobs
-	 *	@since		0.3
-	 */
-	public function user_profile_login()
-	{
-		
-	}
-	
-	/** 
-	 *	
-	 *
-	 *
-	 *
-	 *	@author		Nate Jacobs
-	 *	@since		0.3
-	 */
+	*	Add Brickset Submenu
+	*
+	*	Adds the submenu to the default WordPress settings menu.
+	*
+	*	@author		Nate Jacobs
+	*	@date		2/2/13
+	*	@since		1.0
+	*
+	*	@param		null
+	*/
 	public function add_brickset_submenu()
 	{
-		add_submenu_page(
-			'options-general.php',
-			__( 'Brickset API', 'bs_api' ),
-			__( 'Brickset API', 'bs_api' ),
-			'manage_options',
-			'bs_api_options',
-			array( __CLASS__, 'bs_api_options_page' )
+		add_options_page( 
+			__( 'Brickset API Settings', 'bs_api' ), 
+			__( 'Brickset API', 'bs_api' ), 
+			'manage_options', 
+			'brickset-api-options', 
+			array( __CLASS__, 'bs_api_options_callback' ) 
 		);
 	}
 	
 	/** 
-	 *	
-	 *
-	 *
-	 *
-	 *	@author		Nate Jacobs
-	 *	@since		0.3
-	 */
-	public function bs_api_options_page()
+	*	Brickset Options Page
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		2/2/13
+	*	@since		1.0
+	*
+	*	@param		null
+	*/
+	public function bs_api_options_callback()
 	{
 		?>
 		<div class="wrap">
-			<?php settings_errors(); ?>
 			<div class="icon32" id="icon-options-general"><br></div>
-			<h2><?php __( 'Unofficial Brickset API Settings', 'bs_api' ); ?></h2>
+			<h2><?php _e( 'Brickset API Settings', 'bs_api' ); ?></h2>
+			<?php //settings_errors(); ?>
 			<form method="post" action="options.php">
-				<?php settings_fields( 'user_access_expire_options' ); ?>
-				<?php do_settings_sections( __FILE__ ); ?>
-				<p class="submit">
-				<input type="submit" class="button-primary" value="<?php _e( 'Save API Key', 'bs_api' ) ?>" />
-				</p>
+				<?php settings_fields( 'bs_api_options' ); ?>
+				<?php do_settings_sections( 'brickset-api-options' ); ?>
+				<?php submit_button(); ?>
 			</form>
 		</div>
 		<?php
 	}
+	
+	/** 
+	*	Settings Init
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		2/2/13
+	*	@since		1.0
+	*
+	*	@param		null
+	*/
+	public function settings_init()
+	{
+		add_settings_section( 
+			'bs-webservice-settings', 
+			__( 'API Key', 'bs_api' ), 
+			array( __CLASS__, 'webservice_settings_callback' ), 
+			'brickset-api-options'	 
+		);
+		add_settings_field( 
+			'bs-api-key', 
+			__( 'Enter your API Key', 'bs_api' ), 
+			array( __CLASS__, 'apikey_callback' ), 
+			'brickset-api-options', 
+			'bs-webservice-settings'
+		);
+
+		register_setting( 
+			'bs_api_options', 
+			'brickset-api-settings'
+		);
+	}
+	
+	/** 
+	*	API Key
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		2/2/13
+	*	@since		1.0
+	*
+	*	@param		null
+	*/
+	public function webservice_settings_callback()
+	{
+		echo __( 'You may obtain a key at ', 'bs_api' )."<a href='http://brickset.com/contact/'>Brickset.com</a>";
+
+	}
+	
+	public function apikey_callback() 
+	{
+ 		$settings = (array) get_option( 'brickset-api-settings' );
+		$api_key = isset( $settings['api_key'] ) ? esc_attr( $settings['api_key'] ) : '';
+
+		echo "<input type='text' name='brickset-api-settings[api_key]' value='$api_key' />";
+		
+ 	}
 }
-$brickset_adimin = new BricksetAPIAdmin();
+
+new BricksetAPISettingsPage();
