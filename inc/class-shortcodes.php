4778f7a16cd7087cbfe8bb28eb1596500fac22ce
Nate Jacobs <njelmo@gmail.com>
2013-02-09 00:17:16 -0700

    Add back class extension

--- inc/class-shortcodes.php
+++ inc/class-shortcodes.php
@@ -1,14 +1,12 @@
 <?php
 
-new BricksetAPIShortcode;
-
-class BricksetAPIShortcode
+class BricksetAPIShortcode extends BricksetAPIFunctions
 {
 	public function __construct()
 	{
-		add_shortcode( 'bs_set', array( __CLASS__, 'get_set' ) );
-		add_shortcode( 'bs_my_wanted', array( __CLASS__, 'my_wanted' ) );
-		add_shortcode( 'bs_my_owned', array( __CLASS__, 'my_owned' ) );
+		add_shortcode( 'bs_set', array( $this, 'get_set' ) );
+		add_shortcode( 'bs_my_wanted', array( $this, 'my_wanted' ) );
+		add_shortcode( 'bs_my_owned', array( $this, 'my_owned' ) );
 	}
 	
 	public function get_set( $atts )
@@ -16,20 +14,19 @@ class BricksetAPIShortcode
 		extract( shortcode_atts( array( 
 			'number' 		=> '',
 		), $atts ) );
+
+		parent::get_by_number( $number );
 		
-		$brickset = new BricksetAPIFunctions();
-		$result = $brickset->get_by_number( $number );
-		
-		$return = '<img src="'.$result->imageURL.'"><br>';
-		$return .= '<strong>Set Name: </strong>'.$result->setName.'<br>';
-		$return .= '<strong>Set Number: </strong>'.$result->number.'-'.$result->numberVariant.'<br>';
-		$return .= '<strong>Year: </strong>'.$result->year.'<br>';
-		$return .= '<strong>Theme: </strong>'.$result->theme.'<br>';
-		$return .= '<strong>Subtheme: </strong>'.$result->subtheme.'<br>';
-		$return .= '<strong>US Retail Price: </strong>$'.$result->USRetailPrice.'<br>';
-		$return .= '<strong>Pieces: </strong>'.$result->pieces.'<br>';
-		$return .= '<strong>Minifigs: </strong>'.$result->minifigs.'<br>';
-		$return .= '<strong>Set Guide: </strong><a href='.$result->bricksetURL.'>Brickset</a><br>';
+		$return = '<img src="'.$this->results->setData->imageURL.'"><br>';
+		$return .= '<strong>Set Name: </strong>'.$this->results->setData->setName.'<br>';
+		$return .= '<strong>Set Number: </strong>'.$this->results->setData->number.'-'.$this->results->setData->numberVariant.'<br>';
+		$return .= '<strong>Year: </strong>'.$this->results->setData->year.'<br>';
+		$return .= '<strong>Theme: </strong>'.$this->results->setData->theme.'<br>';
+		$return .= '<strong>Subtheme: </strong>'.$this->results->setData->subtheme.'<br>';
+		$return .= '<strong>US Retail Price: </strong>$'.$this->results->setData->USRetailPrice.'<br>';
+		$return .= '<strong>Pieces: </strong>'.$this->results->setData->pieces.'<br>';
+		$return .= '<strong>Minifigs: </strong>'.$this->results->setData->minifigs.'<br>';
+		$return .= '<strong>Set Guide: </strong><a href='.$this->results->setData->bricksetURL.'>Brickset</a><br>';
 		
 		return $return;
 	}
@@ -39,12 +36,11 @@ class BricksetAPIShortcode
 		global $post;
 		$user_id = $post->post_author;
 		
-		$brickset = new BricksetAPIFunctions();
-		$results = $brickset->get_wanted( $user_id );
+		parent::get_wanted( $user_id );
 		
 		$return = '<h2>My Wanted List</h2>';
 		$return .= '<table><th>Image</th><th>Set Name</th><th>Set Number</th><th>Pieces</th>';	
-		foreach ( $results as $result )
+		foreach ( $this->results as $result )
 		{
 			$return .= '<tr>';
 				$return .= '<td><img src="'.$result->thumbnailURL.'"></td>';
@@ -63,18 +59,18 @@ class BricksetAPIShortcode
 		global $post;
 		$user_id = $post->post_author;
 		
-		$brickset = new BricksetAPIFunctions();
-		$results = $brickset->get_owned( $user_id );
-		
+		parent::get_owned( $user_id );
+
 		$return = '<h2>My Set List</h2>';
-		$return .= '<table><th>Image</th><th>Set Name</th><th>Set Number</th><th>Pieces</th>';	
-		foreach ( $results as $result )
+		$return .= '<table><th>Image</th><th>Set Name</th><th>Set Number</th><th>Pieces</th><th>Quantity</th>';	
+		foreach ( $this->results as $result )
 		{
 			$return .= '<tr>';
 				$return .= '<td><img src="'.$result->thumbnailURL.'"></td>';
 				$return .= '<td>'.$result->setName.'</td>';
 				$return .= '<td>'.$result->number.'</td>';
 				$return .= '<td>'.$result->pieces.'</td>';
+				$return .= '<td>'.$result->qtyOwned.'</td>';
 			$return .= '</tr>';
 		}
 		$return .= '</table>';		
@@ -82,3 +78,4 @@ class BricksetAPIShortcode
 		return $return;
 	}
 }
+new BricksetAPIShortcode;
