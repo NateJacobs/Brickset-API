726c4b07525e1e03f07fda15fc64142d087f4870
Nate Jacobs <njelmo@gmail.com>
2013-03-12 15:11:09 -0700

    Add documentation to oembed. closes #20

--- inc/class-oembed.php
+++ inc/class-oembed.php
@@ -3,7 +3,9 @@
 /** 
 *	Brickset Oembed
 *
-*	
+*	Add oembed support for Brickset theme and set links
+*	Thanks to Lee Willis for the tutorial and code base to work from
+*	https://github.com/leewillis77/wp-wpdotorg-embed
 *
 *	@author		Nate Jacobs
 *	@date		3/9/13
@@ -20,7 +22,7 @@ class BricksetOembed extends BricksetAPIFunctions
 	*	@date		3/9/13
 	*	@since		1.0
 	*
-	*	@param		
+	*	@param		null
 	*/
 	public function __construct()
 	{
@@ -31,30 +33,44 @@ class BricksetOembed extends BricksetAPIFunctions
 	/** 
 	*	Register Oembed
 	*
-	*	
+	*	Register the two URLS that support will be created for
 	*
 	*	@author		Nate Jacobs
 	*	@date		3/9/13
 	*	@since		1.0
 	*
-	*	@param		
+	*	@param		null
 	*/
 	public function register_oembed()
 	{
 		$oembed_url = home_url();
 		$key = $this->get_key();
+		// Create our own oembed url
 		$oembed_url = add_query_arg ( array ( 'brickset_oembed' => $key ), $oembed_url);
 		
 		wp_oembed_add_provider( '#https?://(www\.)?brickset.com/detail/\?Set\=.*/?#i', $oembed_url, true );
 		wp_oembed_add_provider( '#https?://(www\.)?brickset.com/browse/themes/\?theme\=.*/?#i', $oembed_url, true );
 	}
 	
+	/** 
+	*	Get Key
+	*
+	*	Create a random key to prevent hijacking
+	*
+	*	@author		Nate Jacobs
+	*	@date		3/10/13
+	*	@since		1.0
+	*
+	*	@param		null
+	*
+	*	@return		string	$key
+	*/
 	private function get_key() 
 	{
 
 		$key = get_option ( 'brickset_oembed_key' );
 
-		if ( ! $key ) {
+		if ( !$key ) {
 			$key = md5 ( time() . rand ( 0,65535 ) );
 			add_option ( 'brickset_oembed_key', $key, '', 'yes' );
 		}
@@ -63,19 +79,44 @@ class BricksetOembed extends BricksetAPIFunctions
 
 	}
 	
+	/** 
+	*	Maybe Handle Oembed
+	*
+	*	Test if the correct key is present in the URL passed
+	*
+	*	@author		Nate Jacobs
+	*	@date		3/10/13
+	*	@since		1.0
+	*
+	*	@param		null
+	*/
 	public function maybe_handle_oembed() 
 	{
-			if ( isset ( $_GET['brickset_oembed'] ) ) 
-			{
-				return $this->handle_oembed();
-			}
+		// If the query argument is there hand
+		if ( isset ( $_GET['brickset_oembed'] ) ) 
+		{
+			// Hand it off to the handle_oembed function
+			return $this->handle_oembed();
+		}
 	}
-		
+	
+	/** 
+	*	Handle Oembed
+	*
+	*	Takes care of formating the URL to call the Brickset API
+	*
+	*	@author		Nate Jacobs
+	*	@date		3/10/13
+	*	@since		1.0
+	*
+	*	@param		null
+	*/	
 	public function handle_oembed() 
 	{  
-  
+		// Did we get here by mistake?
 	    if ( ! isset ( $_GET['brickset_oembed'] ) ) 
 	    {  
+	    	// If so, get out of here
 	        return;  
 	    }  
 	    // Check this request is valid  
@@ -95,25 +136,35 @@ class BricksetOembed extends BricksetAPIFunctions
 			die( 'Only json allowed' );
 		}
 	    
+	    // Check if URL passed contains the string 'detail/?Set='
 	    if( false !== strpos( $url, 'detail/?Set=' ) )
 	    {
-	    	 $set_id = explode( '?Set=', $url );
-	    
-	    	 if( false !== strpos( $url, '&' ) )
-	    	 	$set_id[1] = strstr( $set_id[1], '&', true );
-	    	 
-	    	 $this->oembed_set( $set_id[1] );
+	    	// If it does, separate on this string
+	    	$set_id = explode( '?Set=', $url );
+	    	
+	    	// Is there additional arguments passed, if so only get what is before the & 
+	    	if( false !== strpos( $url, '&' ) )
+	    		$set_id[1] = strstr( $set_id[1], '&', true );
+	    		
+	    	// Send it on to the Set method 
+	    	$this->oembed_set( $set_id[1] );
 	    }
+	    // Now we are checking if the URL passed contains the string 'browse/themes/?theme='
 	    elseif( false !== strpos( $url, 'browse/themes/?theme=' ) )
 	    {
+	    	// If it does, separate on this string
 	    	$theme_name = explode( '?theme=', $url );
-	    
-	    	 if( false !== strpos( $url, '&' ) )
+	    	
+	    	// Is there additional arguments passed, if so only get what is before the & 
+	    	if( false !== strpos( $url, '&' ) )
 	    	 	$theme_name[1] = strstr( $theme_name[1], '&', true );
+    
+	    	// Convert %20 and other urlencoded strings to non urlencoded
+	    	// The build_query method encodes them later 
+	    	$theme = urldecode( $theme_name[1] );
     	 	
-    	 	$theme = urldecode( $theme_name[1] );
-    	 	
-		   	$this->oembed_theme( $theme );
+    	 	// Send it on to the set method
+    	 	$this->oembed_theme( $theme );
 	    }
 	    else
 	    {
@@ -122,10 +173,23 @@ class BricksetOembed extends BricksetAPIFunctions
 	    }
     }
     
+    /** 
+    *	Oembed Set URL
+    *
+    *	Takes a set number from the Brickset URL and displays information about the set
+    *
+    *	@author		Nate Jacobs
+    *	@date		3/10/13
+    *	@since		1.0
+    *
+    *	@param		string	$set_number
+    */
     public function oembed_set( $set_number )
     {
+    	// Call the API function
     	$brickset = parent::get_by_number( $set_number );
     	
+    	// Build the Oembed class
 	    $response = new stdClass();  
 		$response->type = 'rich';  
 		$response->width = '10';  
@@ -134,13 +198,14 @@ class BricksetOembed extends BricksetAPIFunctions
 		$response->title = $brickset->setData->setName;  
 		$response->html = '<div class="brickset-oembed-set">';
 		
+		// Check for errors
 		if( is_wp_error( $brickset ) )
 		{
 			$response->html .= '<p>'.$brickset->get_error_message().'</p>';
 		}
 		else
 		{
-			//$return = '';
+			// Loop through and display the set information
 			foreach( $brickset as $result )
 			{
 				$response->html .= '<img src="'.$result->imageURL.'"><br>';
@@ -165,21 +230,22 @@ class BricksetOembed extends BricksetAPIFunctions
     }
     
     /** 
-    *	Oembed Theme
+    *	Oembed Theme URL
     *
-    *	
+    *	Takes a theme name from the Brickset URL and displays information about all the sets in that theme in a table
     *
     *	@author		Nate Jacobs
     *	@date		3/10/13
     *	@since		1.0
     *
-    *	@param		
+    *	@param		string	$theme_name
     */
     public function oembed_theme( $theme_name )
     {
-		
+		// Call the API function
 		$brickset = parent::get_by_theme( $theme_name );   
     
+		// Build the Oembed class
     	$response = new stdClass();  
 		$response->type = 'rich';  
 		$response->width = '10';  
@@ -187,15 +253,16 @@ class BricksetOembed extends BricksetAPIFunctions
 		$response->version = '1.0';  
 		$response->title = 'Theme List';  
 		$response->html = '<div class="brickset-oembed-theme">';
-			
+		
+		// Check for errors
 		if( is_wp_error( $brickset ) )
 		{
 			$response->html .= '<p>'.$brickset->get_error_message().'</p>';
 		}
 		else
 		{
-
-			$response->html .= '<table class="brickset-updated-since"><th>'. __( 'Image', 'bs_api' ).'</th><th>'. __( 'Set Name', 'bs_api' ).'</th><th>'. __( 'Set Number', 'bs_api' ).'</th><th>'. __( 'Year', 'bs_api' ).'</th><th>'. __( 'Pieces', 'bs_api' ).'</th>';	
+			// Display the info about the sets in the theme
+			$response->html .= '<table class="brickset-theme-sets"><th>'. __( 'Image', 'bs_api' ).'</th><th>'. __( 'Set Name', 'bs_api' ).'</th><th>'. __( 'Set Number', 'bs_api' ).'</th><th>'. __( 'Year', 'bs_api' ).'</th><th>'. __( 'Pieces', 'bs_api' ).'</th>';	
 
 			foreach ( $brickset as $updated )
 			{
